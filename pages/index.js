d7a2b412348415c2694f41aebe575b1384f820ba
Vrisel <vrisel.p@gmail.com>
2022-01-10 15:43:22 +0900

    STYLE, FIX: index 태그 수정, 내용 일부 수정
    - 대부분의 태그를 mui 컴포넌트로 변경
    - 일부 스타일 적용

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,42 @@ import {
   Link,
   Typography,
 } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import DevIcon from "@mui/icons-material/LogoDev";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import ContactsIcon from "@mui/icons-material/Contacts";
+import EMailIcon from "@mui/icons-material/EMail";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import NoteIcon from "@mui/icons-material/NoteAlt";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import ArticleIcon from "@mui/icons-material/Article";
 
+const useStyles = makeStyles({
+  mbList: {
+    "&>li": {
+      marginBottom: "12px",
+    },
+  },
+  dataList: {
+    "&>dt": {
+      fontWeight: 700,
+      marginBottom: "-0.5em",
+      /* "&::after": {
+        content: ": ",
+      }, */
+    },
+    "&>dd": {
+      marginBottom: "0.5em",
+      paddingLeft: "6em",
+      paddingBottom: "0.5em",
+      borderBottom: "1px solid #000",
+    },
+  },
+});
+
 export default function Home() {
+  const classes = useStyles();
+
   const duolingo = () => {
     const today = new Date();
     const startDay = new Date(2020, 8 - 1, 14);
@@ -26,219 +56,130 @@ export default function Home() {
   return (
     <Layout>
       <Container className="body-font" sx={{ width: "99%" }}>
-        <Box component="section" mb={5} aria-labelledby="home">
-          <Typography
-            component="h2"
-            variant="h4"
-            id="home"
-            className="text-violet"
-          >
+        <Box component="section" mt={2} mb={7} aria-labelledby="home">
+          <Typography component="h2" variant="h4" id="home">
             Hello, World! 🎉
           </Typography>
           <Typography paragraph>
-            백&프론트 풀스택 프로그래머를 지망하는 <strong>Vrisel</strong>
+            프론트&백 풀스택 프로그래머를 지망하는 <strong>Vrisel</strong>
             입니다.
-            <ul>
-              <li>
+            <Box component="ul" className={classes.mbList}>
+              <Typography component="li">
                 <dfn title="새 사도신경">
                   {'"나는 전능하신 아버지 하나님, 천지의 창조주를 믿습니다."'}
                 </dfn>
-              </li>
-              <li>모든 사람이 사람답게 살 수 있는 세상을 꿈꿉니다.</li>
-              <li>
-                프로그래머의 <strong>사회적 역할</strong>에 대해 고민합니다.
-              </li>
-              <li>
-                새로이 배우는 것을 좋아합니다. <br />
-                <strong>배워서 남 주는 사람</strong>이 되고 싶습니다.
-              </li>
-            </ul>
-          </Typography>
-        </Box>
-        <Box component="section" mb={5} aria-labelledby="skills">
-          <Typography
-            component="h2"
-            variant="h5"
-            id="skills"
-            className="text-violet"
-          >
-            <DevIcon />
-            Skills
-          </Typography>
-          <ul>
-            <li>
-              <strong>Front:</strong> Vue, Nuxt
-            </li>
-            <li>
-              <strong>DB:</strong> SQL, MongoDB
-            </li>
-            <li>
-              <strong>Middleware:</strong> Express
-            </li>
-            <li>
-              <strong>Full-stack:</strong> C# ASP.NET Framwork
-            </li>
-          </ul>
-        </Box>
-        <Box component="section" mb={5} aria-labelledby="projects">
-          <Typography
-            component="h2"
-            variant="h5"
-            id="projects"
-            className="text-violet"
-          >
-            <InventoryIcon />
-            Projects
-          </Typography>
-          <Box
-            component="ul"
-            sx={{
-              listStyle: "none",
-              display: "flex",
-              flexWrap: "wrap",
-              alignItems: "stretch",
-            }}
-          >
-            <Box component="li" mr={1} mb={1}>
-              <Card
-                component="article"
-                sx={{ maxWidth: "240px", height: "100%" }}
-              >
-                <CardMedia
-                  component="img"
-                  src="https://via.placeholder.com/240x160"
-                  alt=""
-                />
-                <CardContent>
-                  <Typography component="h3" variant="h6" gutterBottom>
-                    <Link href="https://vrisel.github.io/mss_a11y">
-                      무신사 스토어 클론
-                    </Link>
+              </Typography>
+              <Typography component="li">
+                모든 <strong>사람이 사람답게</strong> 살 수 있는 세상을
+                꿈꿉니다.
+                <Box component="ul">
+                  <Typography component="li">
+                    프로그래머의 <strong>사회적 역할</strong>에 대해 고민합니다.
                   </Typography>
-                  <Typography variant="body2">
-                    : 무신사 스토어 사이트를 이용하며 불편했던 점들을 (특히
-                    접근성 관점에서) 개선해본 프로젝트 (미완성)
+                </Box>
+              </Typography>
+              <Typography component="li">
+                새로이 <strong>배우는 것</strong>을 좋아합니다.
+                <Box component="ul">
+                  <Typography component="li">
+                    <strong>배워서 남 주는 사람</strong>이 되고 싶습니다.
                   </Typography>
-                </CardContent>
-              </Card>
+                </Box>
+              </Typography>
             </Box>
-            <Box component="li" mr={1} mb={1}>
-              <Card
-                component="article"
-                sx={{ maxWidth: "240px", height: "100%" }}
-              >
-                <CardMedia
-                  component="img"
-                  src="https://via.placeholder.com/240x160"
-                  alt=""
-                />
-                <CardContent>
-                  <Typography component="h3" variant="h6" gutterBottom>
-                    <Link href="https://vrisel.github.io/perfect-pitch-challenge">
-                      절대음감 챌린지
-                    </Link>
-                  </Typography>
-                  <Typography variant="body2">
-                    : 주어지는 소리의 음이름을 맞히는 미니 게임
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Box>
-          </Box>
-        </Box>
-        <Box component="section" mb={5} aria-labelledby="contacts">
-          <Typography
-            component="h2"
-            variant="h5"
-            id="contacts"
-            className="text-violet"
-          >
-            <ContactsIcon />
-            Contacts
           </Typography>
-          <address>
-            <ul>
-              <li>
-                E-mail:{" "}
-                <Link href="mailto:vrisel.p@gmail.com">vrisel.p@gmail.com</Link>
-              </li>
-              <li>
-                GitHub:{" "}
-                <Link href="https://github.com/vrisel" target="blank">
-                  Vrisel
-                </Link>
-              </li>
-            </ul>
-          </address>
         </Box>
-        <Box component="section" mb={5} aria-labelledby="tmi">
-          <Typography
-            component="h2"
-            variant="h5"
-            id="tmi"
-            className="text-violet"
-          >
+        <SkillsSection mb={7} dlClass={classes.dataList} />
+        <ProjectsSection mb={7} />
+        <ContactsSection mb={7} />
+        <Box component="section" mb={7} aria-labelledby="tmi">
+          <Typography component="h2" variant="h5" id="tmi">
             <ArticleIcon />
             <abbr title="Too Much Information">TMI</abbr>
           </Typography>
-          <details>
+          <Box component="details">
             <summary>펼치기/접기</summary>
-            <ul>
-              <li>
+            <Box component="ul" className={classes.mbList}>
+              <Box component="li">
                 <strong>Vrisel</strong>은 silver의 애너그램입니다. <br />
-                이름 지을 당시에 사용하던 노트북이 은색이라서...
-              </li>
-              <li>
+                (이름 지을 당시에 사용하던 노트북이 은색)
+              </Box>
+              <Box component="li">
                 가볍게 다뤄본 프로그래밍/스크립팅 언어들:
-                <ul>
-                  <li>
-                    mIRC에서{" "}
+                <Box component="ul">
+                  <Box component="li">
+                    <Link
+                      href="https://www.mirc.com/"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      mIRC
+                    </Link>
+                    에서{" "}
                     <abbr title="mIRC Scripting Language">
                       <strong>mSL</strong>
                     </abbr>
-                  </li>
-                  <li>
+                  </Box>
+                  <Box component="li">
                     Microsoft PowerPoint에서{" "}
                     <abbr title="Visual Basic for Applications">
                       <strong>VBA</strong>
                     </abbr>
-                  </li>
-                  <li>
-                    <abbr title="AutoHotKey">
-                      <strong>AHK</strong>
-                    </abbr>
-                    , <strong>AutoIt</strong>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Typography>
-                  프로그래밍 언어 외에도 각종 언어들을 좋아합니다. (듀오링고{" "}
-                  <u>{duolingo()}일차</u>)
-                </Typography>
-                <ul>
-                  <li>
+                  </Box>
+                  <Box component="li">
+                    <Link
+                      href="https://www.autohotkey.com/"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      AutoHotKey
+                    </Link>
+                    ,{" "}
+                    <Link
+                      href="https://www.autoitscript.com/"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      AutoIt
+                    </Link>
+                  </Box>
+                </Box>
+              </Box>
+              <Box component="li">
+                프로그래밍 언어 외에도 각종 언어들을 좋아합니다.
+                <Box component="ul">
+                  <Box component="li">
+                    <Link
+                      href="https://www.duolingo.com/"
+                      target="_blank"
+                      rel="nofollows"
+                    >
+                      듀오링고
+                    </Link>{" "}
+                    <u>{duolingo()}일차</u>
+                  </Box>
+                  <Box component="li">
                     일상 대화가 가능한 수준:
                     <strong>
                       <dfn title="영어">English</dfn>,{" "}
                       <dfn title="일본어">日本語</dfn>
                     </strong>
-                  </li>
-                  <li>
+                  </Box>
+                  <Box component="li">
                     기초적인 수준:
                     <strong>
                       <dfn title="독일어">Deutsch</dfn>,{" "}
                       <dfn title="중국 표준어">普通话</dfn>
                     </strong>
-                  </li>
-                  <li>
+                  </Box>
+                  <Box component="li">
                     조금 배워본 것들:
                     <strong>
                       <dfn title="스페인어">Español</dfn>,{" "}
                       <dfn title="아랍어">العربية</dfn>
                     </strong>
-                  </li>
-                  <li>
+                  </Box>
+                  <Box component="li">
                     <Link
                       href="https://ko.wikipedia.org/wiki/%EC%9D%B8%EA%B3%B5%EC%96%B4"
                       target="blank"
@@ -257,15 +198,165 @@ export default function Home() {
                         </Link>
                       </dfn>
                     </strong>
-                  </li>
-                </ul>
-              </li>
-              <li>세벌식(3-2015) 자판을 사용합니다.</li>
-              <li>왼손잡이입니다. 식사는 오른손으로 합니다.</li>
-            </ul>
-          </details>
+                  </Box>
+                </Box>
+              </Box>
+              <Box component="li">세벌식(3-2015) 자판을 사용합니다.</Box>
+              <Box component="li">
+                왼손잡이입니다. 식사는 오른손으로 합니다.
+              </Box>
+            </Box>
+          </Box>
         </Box>
       </Container>
     </Layout>
   );
 }
+
+const SkillsSection = (props) => {
+  return (
+    <Box component="section" mb={props.mb} aria-labelledby="skills">
+      <Typography component="h2" variant="h5" id="skills">
+        <DevIcon />
+        Skills
+      </Typography>
+      <Box component="dl" className={props.dlClass}>
+        <Typography component="dt">Front:</Typography>
+        <Typography component="dd">Nuxt(Vue.js), NEXT(React)</Typography>
+        <Typography component="dt">DB:</Typography>
+        <Typography component="dd">SQL Server, MySQL, MongoDB</Typography>
+        <Typography component="dt">Middleware:</Typography>
+        <Typography component="dd">express</Typography>
+        <Typography component="dt">Full-stack:</Typography>
+        <Typography component="dd">C# ASP.NET Framwork</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+const ProjectsSection = (props) => {
+  return (
+    <Box component="section" mb={props.mb} aria-labelledby="projects">
+      <Typography component="h2" variant="h5" id="projects">
+        <InventoryIcon />
+        Projects
+      </Typography>
+      <Box
+        component="ul"
+        sx={{
+          listStyle: "none",
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "stretch",
+        }}
+      >
+        {[
+          {
+            title: "홈페이지",
+            src: "https://via.placeholder.com/240x160",
+            href: "https://vrisel.github.io/",
+            desc: "자기소개 및 포트폴리오 용 홈페이지",
+            skills: "NEXT",
+          },
+          {
+            title: "무신사 스토어 클론",
+            src: "https://via.placeholder.com/240x160",
+            href: "https://vrisel.github.io/mss_a11y",
+            desc: "무신사 스토어 사이트를 이용하며 불편했던 점들을 (특히 접근성 관점에서) 개선해본 프로젝트 (미완성)",
+            skills: "Nuxt, (express), (MySQL)",
+          },
+          {
+            title: "절대음감 챌린지",
+            src: "https://via.placeholder.com/240x160",
+            href: "https://vrisel.github.io/perfect-pitch-challenge",
+            desc: "주어지는 소리의 음이름을 맞히는 미니 게임",
+            skills: "Nuxt",
+          },
+        ].map((atc, index) => (
+          <Box component="li" mr={1} mb={1} key={index}>
+            <Card
+              component="article"
+              sx={{
+                maxWidth: "240px",
+                height: "100%",
+                minHeight: "340px",
+                position: "relative",
+              }}
+            >
+              <CardMedia component="img" src={atc.src} alt="" />
+              <CardContent>
+                <Typography
+                  component="h3"
+                  variant="h6"
+                  align="center"
+                  gutterBottom
+                >
+                  <Link href={atc.href}>{atc.title}</Link>
+                </Typography>
+                <Typography variant="body2" mb={1.5}>
+                  {atc.desc}
+                </Typography>
+                <Typography
+                  my={1}
+                  sx={{ fontSize: "0.8em", position: "absolute", bottom: 0 }}
+                >
+                  사용 기술: {atc.skills}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+const ContactsSection = (props) => {
+  return (
+    <Box component="section" mb={props.mb} aria-labelledby="contacts">
+      <Typography component="h2" variant="h5" id="contacts">
+        <ContactsIcon />
+        Contacts and Links
+      </Typography>
+      <Box component="address">
+        <Box component="ul">
+          {[
+            {
+              title: "E-mail: vrisel.p@gmail.com",
+              icon: <EMailIcon />,
+              href: "mailto:vrisel.p@gmail.com",
+            },
+            {
+              title: "GitHub",
+              icon: <GitHubIcon />,
+              href: "https://www.github.com/vrisel",
+              target: "_blank",
+            },
+            {
+              title: "Velog (공부노트)",
+              icon: <NoteIcon />,
+              href: "https://velog.io/@vrisel",
+              target: "_blank",
+            },
+            {
+              title: "Twitter",
+              icon: <TwitterIcon />,
+              href: "https://www.twitter.com/vrisel_p",
+              target: "_blank",
+            },
+          ].map((item, index) => (
+            <Box component="li" key={index}>
+              {item.icon}
+              <Link
+                href={item.href}
+                {...(item.target && { target: item.target, rel: "noopener" })}
+              >
+                {item.title}
+              </Link>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
