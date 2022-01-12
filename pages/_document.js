9b185b6d366852ec89796f6dfc7c09ae7b8b1ce1
Vrisel <vrisel.p@gmail.com>
2022-01-12 17:30:48 +0900

    FIX: _document에서 `viewport` 관련 태그 삭제
    - 사용하면 안 된다고 함
    - <https://nextjs.org/docs/messages/no-document-viewport-meta>

--- pages/_document.js
+++ pages/_document.js
@@ -29,10 +29,6 @@ class MyDocument extends Document {
         <Head>
           <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
           <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
           <meta name="author" content="Vrisel" />
           <meta name="keywords" content="programmer, introduction, portfolio" />
           <link rel="icon" href="/favicon.ico" />
